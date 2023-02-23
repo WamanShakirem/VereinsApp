4d0be69b36a3c596f541f044ad7691fb693859dc
Waman Shakirem <schwadron@hotmail.com>
2023-02-23 03:31:31 +0100

    Datenbankverbindung hergestellt und die ersten Musterdaten im DataGrid anzeigen lassen.

--- VereinsApp/MainWindow.xaml.cs
+++ VereinsApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +23,27 @@ namespace VereinsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SqlConnection Datenbankverbindung = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\schwa\OneDrive\Dokumente\VereinsApp.mdf;Integrated Security = True; Connect Timeout = 5");
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Datenbankverbindung herstellen
+            Datenbankverbindung.Open();
+
+            string query = "select * from Personendaten";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, Datenbankverbindung);
+
+            DataSet dataSet = new DataSet();
+            sqlDataAdapter.Fill(dataSet);
+
+            PersonenDatenGrid.DataContext = dataSet;
+            PersonenDatenGrid.ItemsSource = dataSet.Tables[0].DefaultView;
+
+
+            Datenbankverbindung.Close();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
